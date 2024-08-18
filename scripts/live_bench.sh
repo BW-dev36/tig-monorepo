618f29692babd38ccd99a19d6f64f95b5072ec60
root <root@m16760.contaboserver.net>
2024-08-18 02:30:31 +0200

    Massive optim

--- scripts/live_bench.sh
+++ scripts/live_bench.sh
@@ -30,30 +30,35 @@ ON_SETTINGS=$1
 
 echo "Building wasm..."
 cargo build -p tig-wasm --target wasm32-wasi --release --features entry-point
-wasm-opt target/wasm32-wasi/release/tig_wasm.wasm -o tig-algorithms/wasm/${CHALLENGE_LOCAL}/${ALGORITHM_LOCAL}.wasm -O2 --remove-imports
+wasm-opt target/wasm32-wasi/release/tig_wasm.wasm -O3 -o tig-algorithms/wasm/${CHALLENGE_LOCAL}/${ALGORITHM_LOCAL}.wasm  --remove-imports
 
 
 # 3. Extraire et itérer sur les paires de qualifier_difficulties pour le challenge 'c004'
 echo "Processing qualifier difficulties for challenge 'c004'..."
 echo "$challenges_response" | jq -c '.challenges[] | select(.id=="c004") | .block_data.qualifier_difficulties[]' | while read -r pair; do
-    echo "Processing pair: $pair"
+
     if [ -n "$ON_SETTINGS" ]; then
+        echo "Processing pair: $ON_SETTINGS"
         SETTINGS="{\"challenge_id\":\"c004\",\"difficulty\":$ON_SETTINGS,\"algorithm_id\":\"c004\",\"player_id\":\"\",\"block_id\":\"\"}"
     else
+        echo "Processing pair: $pair"
         SETTINGS="{\"challenge_id\":\"c004\",\"difficulty\":$pair,\"algorithm_id\":\"c004\",\"player_id\":\"\",\"block_id\":\"\"}"
     fi
+
     start_time=$(date +%s%3N)
-    output=$(./target/release/tig-worker compute_solution $SETTINGS $NONCE $WASM 2>&1)
+    output=$(./target/release/tig-worker compute_solution --fuel 1500000000 $SETTINGS $NONCE $WASM 2>&1)
     exit_code=$?
     end_time=$(date +%s%3N)
     duration=$((end_time - start_time))
     total_ms=$((total_ms + duration))
     if [ $exit_code -eq 0 ]; then
+        echo -e "$output"
         num_solutions=$((num_solutions + 1))
     else
       if echo "$output" | grep -q "Invalid solution\|No solution found"; then
           num_invalid=$((num_invalid + 1))
       else
+          echo -e "$output"
           num_errors=$((num_errors + 1))
       fi
     fi
--- tig-algorithms/src/vector_search/optimax_search/inbound.rs
+++ tig-algorithms/src/vector_search/optimax_search/inbound.rs
@@ -15,6 +15,10 @@ language governing permissions and limitations under the License.
 
 use anyhow::Ok;
 use tig_challenges::vector_search::*;
+use std::time::Instant;
+
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 
 struct KDNode<'a> {
     point: &'a [f32],
@@ -33,32 +37,66 @@ impl<'a> KDNode<'a> {
         }
     }
 }
+fn quickselect_by<F>(arr: &mut [(&[f32], usize)], k: usize, compare: &F)
+where
+    F: Fn(&(&[f32], usize), &(&[f32], usize)) -> Ordering,
+{
+    if arr.len() <= 1 {
+        return;
+    }
+
+    let pivot_index = partition(arr, compare);
+    if k < pivot_index {
+        quickselect_by(&mut arr[..pivot_index], k, compare);
+    } else if k > pivot_index {
+        quickselect_by(&mut arr[pivot_index + 1..], k - pivot_index - 1, compare);
+    }
+}
+
+fn partition<F>(arr: &mut [(&[f32], usize)], compare: &F) -> usize
+where
+    F: Fn(&(&[f32], usize), &(&[f32], usize)) -> Ordering,
+{
+    let pivot_index = arr.len() >> 1;
+    arr.swap(pivot_index, arr.len() - 1);
+
+    let mut store_index = 0;
+    for i in 0..arr.len() - 1 {
+        if compare(&arr[i], &arr[arr.len() - 1]) == Ordering::Less {
+            arr.swap(i, store_index);
+            store_index += 1;
+        }
+    }
+    arr.swap(store_index, arr.len() - 1);
+    store_index
+}
 
 fn build_kd_tree<'a>(points: &mut [(&'a [f32], usize)]) -> Option<Box<KDNode<'a>>> {
     if points.is_empty() {
         return None;
     }
 
-    let num_dimensions = points[0].0.len(); // Nombre total de dimensions des vecteurs
+    const NUM_DIMENSIONS: usize = 250;
     let mut stack: Vec<(usize, usize, usize, Option<*mut KDNode<'a>>, bool)> = Vec::new();
     let mut root: Option<Box<KDNode<'a>>> = None;
 
     stack.push((0, points.len(), 0, None, false));
-
-    let mut axis = 0;
 
     while let Some((start, end, depth, parent_ptr, is_left)) = stack.pop() {
         if start >= end {
             continue;
         }
 
-        let median = (start + end) >> 1;
+        let axis = depth % NUM_DIMENSIONS;
+        let median = (start + end) / 2;
 
-        points[start..end].sort_unstable_by(|a, b| a.0[axis].partial_cmp(&b.0[axis]).unwrap());
+        // Need to implement QuicSort or better QuickSelect
+        quickselect_by(&mut points[start..end], median - start, &|a, b| {
+            a.0[axis].partial_cmp(&b.0[axis]).unwrap()
+        });
 
         let (median_point, median_index) = points[median];
         let mut new_node = Box::new(KDNode::new(median_point, median_index));
-
         let new_node_ptr: *mut KDNode = &mut *new_node;
 
         if let Some(parent_ptr) = parent_ptr {
@@ -75,15 +113,11 @@ fn build_kd_tree<'a>(points: &mut [(&'a [f32], usize)]) -> Option<Box<KDNode<'a>
 
         stack.push((median + 1, end, depth + 1, Some(new_node_ptr), false));
         stack.push((start, median, depth + 1, Some(new_node_ptr), true));
-
-        axis += 1;
-        if axis >= num_dimensions {
-            axis = 0;
-        }
     }
 
     root
 }
+
 
 fn squared_euclidean_distance(a: &[f32], b: &[f32]) -> f32 {
     let mut sum = 0.0;
@@ -94,15 +128,38 @@ fn squared_euclidean_distance(a: &[f32], b: &[f32]) -> f32 {
     sum
 }
 
+#[inline(always)]
 fn early_stopping_distance(a: &[f32], b: &[f32], current_min: f32) -> f32 {
     let mut sum = 0.0;
-    for i in 0..a.len() {
-        let diff = a[i] - b[i];
-        sum += diff * diff;
+    let mut i = 0;
+
+    //Try to unroll for simd vectorization
+    while i + 3 < a.len() {
+        let diff0 = a[i] - b[i];
+        let diff1 = a[i + 1] - b[i + 1];
+        let diff2 = a[i + 2] - b[i + 2];
+        let diff3 = a[i + 3] - b[i + 3];
+
+        sum += diff0 * diff0 + diff1 * diff1 + diff2 * diff2 + diff3 * diff3;
+
         if sum > current_min {
             return f32::MAX;
         }
+
+        i += 4;
     }
+
+    while i < a.len() {
+        let diff = a[i] - b[i];
+        sum += diff * diff;
+
+        if sum > current_min {
+            return f32::MAX;
+        }
+
+        i += 1;
+    }
+
     sum
 }
 
@@ -128,27 +185,32 @@ fn nearest_neighbor_search<'a>(
         }
 
         let diff = target[axis] - node.point[axis];
-        let (nearer, farther) = if diff < 0.0 {
-            (&node.left, &node.right)
-        } else {
-            (&node.right, &node.left)
-        };
+        let sqr_diff = diff * diff;
 
-        if let Some(farther_node) = farther {
-            if diff * diff < best.0 {
+        if sqr_diff < best.0 {
+            if let Some(farther_node) = if diff < 0.0 {
+                &node.right
+            } else {
+                &node.left
+            } {
                 stack.push((farther_node.as_ref(), depth + 1));
             }
         }
 
-        if let Some(nearer_node) = nearer {
+        if let Some(nearer_node) = if diff < 0.0 {
+            &node.left
+        } else {
+            &node.right
+        } {
             stack.push((nearer_node.as_ref(), depth + 1));
         }
     }
 }
 
+
 fn calculate_mean_vector(vectors: &[&[f32]]) -> Vec<f32> {
     let num_vectors = vectors.len();
-    let num_dimensions = vectors[0].len();
+    let num_dimensions = 250;
 
     let mut mean_vector = vec![0.0; num_dimensions];
 
@@ -165,7 +227,64 @@ fn calculate_mean_vector(vectors: &[&[f32]]) -> Vec<f32> {
     mean_vector
 }
 
+#[derive(Debug)]
+struct FloatOrd(f32);
+
+impl PartialEq for FloatOrd {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl Eq for FloatOrd {}
+
+impl PartialOrd for FloatOrd {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl Ord for FloatOrd {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Inverser l'ordre ici si nécessaire pour max-heap ou min-heap
+        self.partial_cmp(other).unwrap_or(Ordering::Equal)
+    }
+}
+
 fn filter_relevant_vectors<'a>(
+    database: &'a [Vec<f32>],
+    query_vectors: &[Vec<f32>],
+    k: usize,
+) -> Vec<(&'a [f32], usize)> {
+    let query_refs: Vec<&[f32]> = query_vectors.iter().map(|v| &v[..]).collect();
+    let mean_query_vector = calculate_mean_vector(&query_refs);
+
+    let mut heap: BinaryHeap<(FloatOrd, usize)> = BinaryHeap::with_capacity(k);
+
+    for (index, vector) in database.iter().enumerate() {
+        let dist = squared_euclidean_distance(&mean_query_vector, vector);
+        let ord_dist = FloatOrd(dist);
+        if heap.len() < k {
+            heap.push((ord_dist, index));
+        } else if let Some(&(FloatOrd(top_dist), _)) = heap.peek() {
+            if dist < top_dist {
+                heap.pop();
+                heap.push((ord_dist, index));
+            }
+        }
+    }
+
+    // Extract results from the heap
+    let mut result: Vec<(&'a [f32], usize)> = heap
+        .into_iter()
+        .map(|(_, index)| (&database[index][..], index))
+        .collect();
+
+    result
+}
+
+
+fn filter_relevant_vectors_old<'a>(
     database: &'a [Vec<f32>],
     query_vectors: &[Vec<f32>],
     k: usize,
@@ -191,7 +310,10 @@ fn filter_relevant_vectors<'a>(
         .collect()
 }
 
+
 pub fn solve_challenge(challenge: &Challenge) -> anyhow::Result<Option<Solution>> {
+    let start_total = Instant::now();
+
     let query_count = challenge.query_vectors.len();
 
     let subset_size = match query_count {
@@ -200,22 +322,29 @@ pub fn solve_challenge(challenge: &Challenge) -> anyhow::Result<Option<Solution>
         20..=28 if challenge.difficulty.better_than_baseline <= 465 => 3000,
         20..=28 if challenge.difficulty.better_than_baseline > 465 => 3000, // need more fuel
         29..=50 if challenge.difficulty.better_than_baseline <= 480 => 2000,
-        29..=50 if challenge.difficulty.better_than_baseline > 480 => 2000, // need more fuel
+        29..=50 if challenge.difficulty.better_than_baseline > 480 => 3000, // need more fuel
         51..=70 if challenge.difficulty.better_than_baseline <= 480 => 1300,
-        51..=70 if challenge.difficulty.better_than_baseline > 480 => 1300, // need more fuel
+        51..=70 if challenge.difficulty.better_than_baseline > 480 => 3000, // need more fuel
         71..=100 if challenge.difficulty.better_than_baseline <= 445 => 1000,
         71..=100 if challenge.difficulty.better_than_baseline > 445 => 1000, // need more fuel
-        _ => 1000,                                                           // need more fuel
+        _ => 15000,                                                           // need more fuel
     };
 
+    let start_filter = Instant::now();
     let subset = filter_relevant_vectors(
         &challenge.vector_database,
         &challenge.query_vectors,
         subset_size,
     );
+    let duration_filter = start_filter.elapsed();
+    println!("Time taken for filtering relevant vectors: {:?}", duration_filter);
 
+    let start_build_kd_tree = Instant::now();
     let kd_tree = build_kd_tree(&mut subset.clone());
+    let duration_build_kd_tree = start_build_kd_tree.elapsed();
+    println!("Time taken for building KD-Tree: {:?}", duration_build_kd_tree);
 
+    let start_search = Instant::now();
     let mut best_indexes = Vec::with_capacity(challenge.query_vectors.len());
 
     for query in challenge.query_vectors.iter() {
@@ -226,6 +355,11 @@ pub fn solve_challenge(challenge: &Challenge) -> anyhow::Result<Option<Solution>
             best_indexes.push(best_index);
         }
     }
+    let duration_search = start_search.elapsed();
+    println!("Time taken for nearest neighbor search: {:?}", duration_search);
+
+    let total_duration = start_total.elapsed();
+    println!("Total time taken by solve_challenge: {:?}", total_duration);
 
     Ok(Some(Solution {
         indexes: best_indexes,
