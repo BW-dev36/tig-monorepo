323218825abe14ff77eea565d04e0852387c3fdd
agrimLake <cryptagrim@gmail.com>
2024-09-07 21:45:40 +0200

    Adapt python

--- tig-benchmarker/main.py
+++ tig-benchmarker/main.py
@@ -1,4 +1,5 @@
 import asyncio
+from signal import SIGINT, SIGTERM
 from typing import Dict, Any
 from master.data import *
 from master import (
@@ -11,22 +12,30 @@ from master import (
 )
 
 async def main():
-    state = State(
-        query_data=await data_fetcher._execute(),
-        available_jobs={},
-        pending_benchmark_jobs={},
-        pending_proof_jobs={},
-        submitted_proof_ids=set(),
-        difficulty_samplers={}
-    )
-    await asyncio.gather(*[
-        job_manager.run(state),
-        data_fetcher.run(state),
-        webserver.run(state),
-        recomputer.run(state),
-        benchmark_submitter.run(state),
-        proof_submitter.run(state)
-    ])
+    try:
+        state = State(
+            query_data=await data_fetcher._execute(),
+            available_jobs={},
+            pending_benchmark_jobs={},
+            pending_proof_jobs={},
+            submitted_proof_ids=set(),
+            difficulty_samplers={}
+        )
+        await asyncio.gather(*[
+            job_manager.run(state),
+            data_fetcher.run(state),
+            webserver.run(state),
+            recomputer.run(state),
+            benchmark_submitter.run(state),
+            proof_submitter.run(state)
+        ])
+    except asyncio.CancelledError:
+        do_cleanup()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    coro = main()
+    try:
+        loop.run_until_complete(coro)
+    except KeyboardInterrupt:
+        print("Received exit, exiting")
